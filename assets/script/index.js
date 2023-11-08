f26a8171d56e6a686cd85298f7fc4c617bf657fd
nnifemi <odeyeminifemi@icloud.com>
2023-11-08 11:06:03 -0600

    tic-tac-toe

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,8 @@
 let playerText = document.getElementById('playerText')
 let restartButton = document.getElementById('restartButton')
 let boxes = Array.from(document.getElementsByClassName('box'))
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
+
 
 const O_Player = "O"
 const X_Player = "X"
@@ -17,9 +19,38 @@ function boxClicked(e) {
     if(!spaces[id]){
         spaces[id] = currentPlayer
         e.target.innerText = currentPlayer
+        if(playerHasWon() !==false){
+            playerText.innerHTML = `${currentPlayer} has won`
+            let winning_blocks = playerHasWon()
+
+            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
+            return
+        }
 
         currentPlayer = currentPlayer == X_Player ? O_Player : X_Player
     }
+}
+
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+function playerHasWon() {
+    for (const condition of winningCombos) {
+        let [a, b, c] = condition
+
+        if (spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            return [a,b,c]
+        }
+    }
+    return false;
 }
 
 restartButton.addEventListener('click', restart)
@@ -28,6 +59,11 @@ function restart() {
     spaces.fill(null)
     boxes.forEach(box => {
         box.innerText = ''
+        box.style.backgroundColor=''
     })
+
+    playerText.innerHTML = 'Tic Tac Toe'
+
+    currentPlayer = X_Player
 }
 startGame()
